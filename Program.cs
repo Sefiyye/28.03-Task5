ba09f2e7279d64599d73d5668185bc1fcedae873
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-28 09:10:29 +0400

    Task5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _28._03_Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 2, 8, 10, 15, 18, 20, 12 };
+            int NumbersYouWillAdd = 16;
+            ToAddANewElement(numbers, NumbersYouWillAdd);
+
+        }
+        public static void ToAddANewElement(int [] arr,int num)
+        {
+            Array.Resize(ref arr,arr.Length + 1);
+            arr[arr.Length - 1] = num;
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
